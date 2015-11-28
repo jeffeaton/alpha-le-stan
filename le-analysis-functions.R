e410849fb2473abd6d597e09448d3d769a6352b9
jeffeaton <jeffrey.eaton@imperial.ac.uk>
2015-11-28 15:36:50 +0200

    added additional functions to le-analysis-functions.R

--- le-analysis-functions.R
+++ le-analysis-functions.R
@@ -129,6 +129,23 @@ calc.le <- function(psurv, dt){
   dt*colSums(apply(1.0 - (psurv.last-psurv.curr)/psurv.last, 1, cumprod))
 }
 
+calc.cumincid <- function(param, stand, years=diff(range(mod$stand$x_age))){
+  aidx <- seq_len(years/stand$dt)
+  log_incrate_time_age <- stand$X_incrate_time %*% param$coef_incrate_time_age %*% t(stand$Xmid_incrate_age[aidx,])
+  cumincid.period <- 1.0 - exp(-stand$dt * rowSums(exp(log_incrate_time_age)))
+  setNames(cumincid.period, stand$x_time)
+}
+
+calc.incid <- function(param, stand){ exp(stand$X_incrate_time %*% param$coef_incrate_time_age %*% t(stand$X_incrate_age)) }
+
+calc.prev <- function(param, stand){
+  modpred <- create.modpred(param, stand)
+  phivn <- Rcreate_phivn_mat(modpred)
+  phivp <- Rcreate_phivp_mat(modpred, stand)
+  return(phivp/(phivn+phivp))
+}
+
+
 
 ###############################
 ####  Aggregate functions  ####
@@ -169,5 +186,109 @@ add.mx <- function(mod){
        q4515       = sapply(lapply(psurvobj, "[[", "psurv"), calc.45q15, mod$stand$dt),
        q4515.noart = sapply(lapply(psurvobj, "[[", "psurv.noart"), calc.45q15, mod$stand$dt),
        q4515.nohiv = sapply(lapply(psurvobj, "[[", "psurv.nohiv"), calc.45q15, mod$stand$dt))
+}
+
+
+who.standard.pop <- setNames(c(179177, 178022, 177011, 176133, 175366, 174700, 174144, 173666, 173277, 172933, 172633, 172333, 171999, 171600, 171155, 170699, 170188, 169522, 168666, 167655, 166566, 165466, 164344, 163233, 162122, 160944, 159711, 158511, 157355, 156211, 155044, 153766, 152333, 150700, 148900, 147022, 145100, 143077, 140911, 138655, 136299, 133911, 131633, 129499, 127455, 125388, 123200, 120911, 118455, 115855, 113200, 110488, 107577, 104433, 101133, 9774, 9435, 9095, 8757, 8423, 8087, 7750, 7425, 7113, 6812, 6517, 6221, 5923, 5618, 5311, 5005, 4706, 4412, 4125, 3844, 3569, 3300, 3035, 2773, 2518, 2271, 2033, 1807, 1594, 1203, 1025, 863, 718, 589, 475, 374, 287, 212, 152, 106, 76, 62, 66, 90, 50), 0:99)
+
+
+
+add.incprev <- function(mod){
+  param <- create.param.list(mod$fit)
+
+  ## prev
+  system.time(prev <- mclapply(param, calc.prev, mod$stand))
+  system.time(prev <- array(unlist(prev), c(mod$stand$STEPS_time, mod$stand$STEPS_age, length(param))))
+  dimnames(prev) <- list(mod$stand$x_time, mod$stand$x_age, NULL)
+  mod$prev <- prev
+
+  ## incid
+  system.time(incid <- lapply(param, calc.incid, mod$stand))
+  incid <- array(unlist(incid), c(mod$stand$STEPS_time, mod$stand$STEPS_age, length(param)))
+  dimnames(incid) <- list(mod$stand$x_time, mod$stand$x_age, NULL)
+  mod$incid <- incid
+
+  ## cumincid
+  mod$cumincid <- sapply(param, calc.cumincid, mod$stand, years=45) # incidence between age 15 and 60
+
+  ## WHO prev
+  aidx <- 1:(35/mod$stand$dt+1L)
+  age.dist <- approx(0:99+0.5, who.standard.pop, mod$stand$x_age[aidx])$y  # prevalence age 15 to 50
+  age.dist <- age.dist/sum(age.dist)
+  mod$who15to49prev <- apply(sweep(mod$prev[, aidx,], 2, age.dist, "*"), c(1,3), sum)
+
+  return(mod)
+}
+
+
+##########################
+####  Plot functions  ####
+##########################
+
+cred.region <- function(x, y, ...)
+  polygon(c(x, rev(x)), c(y[1,], rev(y[2,])), border=NA, ...)
+
+transp <- function(col, alpha=0.5)
+  return(apply(col2rgb(col), 2, function(c) rgb(c[1]/255, c[2]/255, c[3]/255, alpha)))
+
+plot.timebysex <- function(m.post, f.post, main, ylim=c(0,1), xlim=c(1990, 2015), ylab=""){
+  par(tcl=-0.25, las=1, mgp=c(2, 0.5, 0), mar=c(2.1, 3.1, 2.1, 1.1))
+  xx <- as.numeric(rownames(m.post))
+  matplot(xx, cbind(apply(m.post, 1, median), apply(f.post, 1, median)),
+        type="l", ylim=ylim, xlim=xlim, col=c("royalblue", "deeppink"), lty=1, lwd=1.5,
+        main=main, xlab="", ylab=ylab)
+  cred.region(xx, apply(m.post, 1, quantile, c(0.025, 0.975)), col=transp("royalblue"))
+  cred.region(xx, apply(f.post, 1, quantile, c(0.025, 0.975)), col=transp("deeppink"))
+  legend("topleft", legend=c("men", "women"), col=c("royalblue", "deeppink"), lty=1.5, bg="white", cex=0.8)
+}
+
+plot.ci <- function(x, y, se=NULL, lower=NULL, upper=NULL, col=1, pch=20, cex=0.8, lty=1, lwd=1){
+  points(x, y, pch=pch, col=col, cex=cex)
+  segments(x0=x, y0=if(is.null(lower)) y-qnorm(0.975)*se else lower, y1=if(is.null(upper)) y+qnorm(0.975)*se else upper, col=col, lty=lty, lwd=lwd)
+}
+
+
+plot.le <- function(le, stand, main, ylim=c(35, 65), xlim=c(1980, 2015)){
+  par(las=1, tcl=-0.25, mgp=c(2, 0.5, 0), mar=c(2.1, 3.1, 2.1, 0.5))
+  ##
+  xx <- stand$x_time[-1]
+  idx.le <- 2:stand$STEPS_time - 1
+  idx.noart <- stand$artstart_tIDX:stand$STEPS_time-1
+  idx.nohiv <- which(xx %in% stand$x_natmx)
+  ##
+  plot(xx[idx.le], rowMeans(le$le[idx.le,]), type='n', lty=1, ylim=ylim, xlim=xlim,
+       ylab=expression(e[15]), xlab="", main=main)
+  ##
+  cred.region(xx[idx.noart], apply(le$le.noart[idx.noart,], 1, quantile, c(0.025, 0.975)), col=transp(2))
+  cred.region(xx[idx.nohiv], apply(le$le.nohiv[idx.nohiv,], 1, quantile, c(0.025, 0.975)), col=transp(3))
+  cred.region(xx[idx.le], apply(le$le[idx.le,], 1, quantile, c(0.025, 0.975)), col=transp(1))
+  ##
+  lines(xx[idx.noart], rowMeans(le$le.noart[idx.noart,]), col=2, lwd=1.5)
+  lines(xx[idx.nohiv], rowMeans(le$le.nohiv[idx.nohiv,]), col=3, lwd=1.5)
+  lines(xx[idx.le], rowMeans(le$le[idx.le,]), col=1, lwd=1.5)
+  ##
+  return(invisible(NULL))
+}
+
+plot.45q15 <- function(q4515, stand, main, ylim=c(0, 0.8), xlim=c(1980, 2015)){
+  par(las=1, tcl=-0.25, mgp=c(2, 0.5, 0), mar=c(2.1, 3.1, 2.1, 0.5))
+  ##
+  xx <- stand$x_time[-1]
+  idx.est <- 2:stand$STEPS_time - 1
+  idx.noart <- stand$artstart_tIDX:stand$STEPS_time-1
+  idx.nohiv <- which(xx %in% stand$x_natmx)
+  ##
+  plot(xx[idx.est], rowMeans(q4515$q4515[idx.est,]), type='n', lty=1, ylim=ylim, xlim=xlim,
+       ylab=expression(""[45]~q[15]), xlab="", main=main)
+  ##
+  cred.region(xx[idx.noart], apply(q4515$q4515.noart[idx.noart,], 1, quantile, c(0.025, 0.975)), col=transp(2))
+  cred.region(xx[idx.nohiv], apply(q4515$q4515.nohiv[idx.nohiv,], 1, quantile, c(0.025, 0.975)), col=transp(3))
+  cred.region(xx[idx.est], apply(q4515$q4515[idx.est,], 1, quantile, c(0.025, 0.975)), col=transp(1))
+  ##
+  lines(xx[idx.noart], rowMeans(q4515$q4515.noart[idx.noart,]), col=2, lwd=1.5)
+  lines(xx[idx.nohiv], rowMeans(q4515$q4515.nohiv[idx.nohiv,]), col=3, lwd=1.5)
+  lines(xx[idx.est], rowMeans(q4515$q4515[idx.est,]), col=1, lwd=1.5)
+  ##
+  return(invisible(NULL))
 }
 
